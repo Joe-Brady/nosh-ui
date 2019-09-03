45698a15a8b339bd464aa7c999f29386ddf2ab77
Joe Brady <joe.brady@uk.ibm.com>
2019-09-03 13:52:50 +0100

    Add Normalize CSS

--- src/ThemeWrapper.tsx
+++ src/ThemeWrapper.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { createGlobalStyle } from "styled-components";
+import NormalizeCSS from "./assets/NormalizeCSS";
+
 import theme from "./theme";
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Vesper Libre';
+    src: url('/static/VesperLibre-Regular.ttf');
+  }
+
+  ${NormalizeCSS};
+
   html, body {
+    font-family: 'Vesper Libre', sans-serif;
     background-color: ${theme.example};
   }
 `;
